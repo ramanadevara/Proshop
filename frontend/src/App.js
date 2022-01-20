1f04780cf33ad1f9cc60146d804532c438f82180
unknown <venkatdevara98@gmail.com>
2022-01-20 18:27:26 +0530

    Adds routing to product page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,22 @@ import Header from "./components/Header"
 import Footer from "./components/Footer"
 import { Container } from "react-bootstrap"
 import HomeScreen from "./Screens/HomeScreen"
+import ProductScreen from "./Screens/ProductScreen"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 const App = () => {
   return (
-    <>
+    <Router>
       <Header></Header>
       <Container>
         <main>
-          <HomeScreen></HomeScreen>
+          <Routes>
+            <Route path='/' element={<HomeScreen />} />
+            <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
         </main>
       </Container>
       <Footer></Footer>
-    </>
+    </Router>
   )
 }
 
