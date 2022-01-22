53aa1b4d970f1630ceed8e13b586edb514c1534b
unknown <venkatdevara98@gmail.com>
2022-01-22 19:01:43 +0530

    sets up MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from "express"
 import products from "./data/products.js"
 import dotenv from "dotenv"
+import connectDB from "./config/db.js"
 
 dotenv.config()
+connectDB()
 
 const PORT = process.env.PORT
 
