8127afd42ecab9017b2e4fc2b5a8885d954072e2
unknown <venkatdevara98@gmail.com>
2022-01-25 15:24:08 +0530

    adds custom error handling

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,18 @@ import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./config/db.js"
 import productRouter from "./routes/productRoutes.js"
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
+
 dotenv.config()
 connectDB()
 
 const PORT = process.env.PORT
 
 const app = express()
+
+app.use(errorHandler)
+
+app.use(notFound)
 
 app.listen(PORT, console.log(`Server is running on Port ${PORT}`))
 
